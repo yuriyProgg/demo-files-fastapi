c01c10d961ac6c2b691f6fd6a5bdd6d51fa60108
Yuri Masalov <masalov.09@bk.ru>
2025-08-31 00:40:02 +0600

    Added new middleware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from posts import post_router
 from media import media_router
 
@@ -10,5 +11,16 @@ app = FastAPI(
     root_path="/api",
 )
 
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Разрешаем только ваш frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Разрешаем все методы (GET, POST, и т.д.)
+    allow_headers=["*"],  # Разрешаем все заголовки
+)
+
+
+# Роутеры
 app.include_router(media_router)
 app.include_router(post_router)
